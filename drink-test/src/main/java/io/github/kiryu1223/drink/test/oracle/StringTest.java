e553c718157ed45dff8ad8464cab88045c94ac56
kiryu1223 <1085684963@qq.com>
2024-09-08 12:46:28 +0800

    增加案例

--- drink-test/src/main/java/io/github/kiryu1223/drink/test/oracle/StringTest.java
+++ drink-test/src/main/java/io/github/kiryu1223/drink/test/oracle/StringTest.java
@@ -15,27 +15,27 @@ public class StringTest extends OracleTest
     @Test
     public void concat()
     {
-        List<String> c1 = client.queryEmptyTable()
+        String c1 = client.queryEmptyTable()
                 .endSelect(() -> SqlFunctions.concat("aa", "bb"))
-                .toList();
-        Assert.assertEquals(c1.get(0),"aabb");
-        List<String> c2 = client.queryEmptyTable()
+                .getOne();
+        Assert.assertEquals(c1,"aabb");
+        String c2 = client.queryEmptyTable()
                 .endSelect(() -> SqlFunctions.concat("aa", "bb", "cc", "dd", "ee", "ff", "gg"))
-                .toList();
-        Assert.assertEquals(c2.get(0),"aabbccddeeffgg");
+                .getOne();
+        Assert.assertEquals(c2,"aabbccddeeffgg");
     }
 
     @Test
     public void join()
     {
-        List<String> j1 = client.queryEmptyTable()
+        String j1 = client.queryEmptyTable()
                 .endSelect(() -> SqlFunctions.join("+", "aa", "bb"))
-                .toList();
-        Assert.assertEquals(j1.get(0),"aa+bb");
-        List<String> j2 = client.queryEmptyTable()
+                .getOne();
+        Assert.assertEquals(j1,"aa+bb");
+        String j2 = client.queryEmptyTable()
                 .endSelect(() ->
                         SqlFunctions.join("|", "aa", "bb", "cc", "dd", "ee", "ff", "gg")
-                ).toList();
-        Assert.assertEquals(j2.get(0),"aa|bb|cc|dd|ee|ff|gg");
+                ).getOne();
+        Assert.assertEquals(j2,"aa|bb|cc|dd|ee|ff|gg");
     }
 }
