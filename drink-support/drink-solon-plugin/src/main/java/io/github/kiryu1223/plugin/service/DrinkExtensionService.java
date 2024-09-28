c552b9303632aee107e5a37e040ae66d5ff68655
kiryu1223 <1085684963@qq.com>
2024-09-28 23:24:17 +0800

    1

--- drink-support/drink-solon-plugin/src/main/java/io/github/kiryu1223/plugin/service/DrinkExtensionService.java
+++ drink-support/drink-solon-plugin/src/main/java/io/github/kiryu1223/plugin/service/DrinkExtensionService.java
@@ -1,10 +1,17 @@
 package io.github.kiryu1223.plugin.service;
 
 import com.sun.source.util.TaskEvent;
+import com.sun.tools.javac.util.Context;
 import io.github.kiryu1223.expressionTree.ext.IExtensionService;
 
 public class DrinkExtensionService implements IExtensionService
 {
+    @Override
+    public void init(Context context)
+    {
+
+    }
+
     @Override
     public void started(TaskEvent event) throws Throwable
     {
