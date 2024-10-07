2b0c03c5fd09053ab76b938643e37d8c79c46e9d
kiryu1223 <1085684963@qq.com>
2024-10-07 20:51:02 +0800

    增加java内置函数转换支持

--- drink-support/drink-aot/src/main/java/io/github/kiryu1223/aot/service/DrinkExtensionService.java
+++ drink-support/drink-aot/src/main/java/io/github/kiryu1223/aot/service/DrinkExtensionService.java
@@ -33,7 +33,7 @@ import java.util.Set;
 
 public class DrinkExtensionService implements IExtensionService
 {
-    private static final String projectVersion = "1.0.7";
+    private static final String projectVersion = "1.0.8";
     private FileObject aotConfig;
     private boolean aotTime;
     private Set<String> AnonymousClassesName = new HashSet<>();
