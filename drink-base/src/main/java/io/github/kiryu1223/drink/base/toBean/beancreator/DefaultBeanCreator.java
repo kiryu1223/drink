c10c0385552e8fba5944670ad4950e131dc5e429
FANGYUE <1085684963@qq.com>
2025-06-12 19:43:42 +0800

    .

--- drink-base/src/main/java/io/github/kiryu1223/drink/base/toBean/beancreator/DefaultBeanCreator.java
+++ drink-base/src/main/java/io/github/kiryu1223/drink/base/toBean/beancreator/DefaultBeanCreator.java
@@ -63,7 +63,6 @@ public class DefaultBeanCreator<T> extends AbsBeanCreator<T> {
                     constructorHandle.type(), 1);
             return (Supplier<T>) site.getTarget().invokeExact();
         } catch (Throwable e) {
-            System.out.println(target);
             throw new DrinkException(e);
         }
     }
