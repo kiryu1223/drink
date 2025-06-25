68c1ade0172cd6ae5e96681fd694b865259af6d1
kiryu1223 <1085684963@qq.com>
2025-06-25 18:54:52 +0800

    1.0.26

--- drink-base/src/main/java/io/github/kiryu1223/drink/base/annotation/IJsonObject.java
+++ drink-base/src/main/java/io/github/kiryu1223/drink/base/annotation/IJsonObject.java
@@ -6,8 +6,7 @@ import io.github.kiryu1223.drink.base.exception.Winner;
  * 标记为数据库json对象
  */
 public interface IJsonObject {
-   default <T> T get(String key)
-   {
-       return Winner.error();
-   }
+//   default <T> T get(String key) {
+//       return Winner.error();
+//   }
 }
