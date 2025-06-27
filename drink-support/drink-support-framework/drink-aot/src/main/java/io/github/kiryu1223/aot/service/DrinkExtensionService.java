385aeb4f569299aa2ed80e03b93a8bf085629649
kiryu1223 <1085684963@qq.com>
2025-06-27 23:46:33 +0800

    1.0.28

--- drink-support/drink-support-framework/drink-aot/src/main/java/io/github/kiryu1223/aot/service/DrinkExtensionService.java
+++ drink-support/drink-support-framework/drink-aot/src/main/java/io/github/kiryu1223/aot/service/DrinkExtensionService.java
@@ -32,7 +32,7 @@ import java.util.Set;
 
 public class DrinkExtensionService implements IExtensionService
 {
-    private static final String projectVersion = "1.0.27";
+    private static final String projectVersion = "1.0.28";
     private FileObject aotConfig;
     private boolean aotTime;
     private Set<String> AnonymousClassesName = new HashSet<>();
